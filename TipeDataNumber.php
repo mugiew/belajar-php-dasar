6ee703eb6b429c4c65dfccd31b316f4027a8ad30
John Doe <johndoe@example.com>
2025-08-10 17:32:45 +0700

    First commit

---
+++ TipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+echo 'Decimal: ';
+var_dump(1234);
+
+echo 'Octal: ';
+var_dump(0123);
+
+echo 'Hexadecimal: ';
+var_dump(0x1a);
+
+echo 'Binary: ';
+var_dump(0b11111111);
+
+echo 'Underscore di Number: ';
+var_dump(1_234_567);
+
+echo 'Floating point: ';
+var_dump(1.234);
+
+echo 'Floating point dengan E notation Plus (1.2 x 1000): ';
+var_dump(1.2e3);
+
+echo 'Floating point dengan E notation Minus (7 x 0.001): ';
+var_dump(7e-3);
+
+echo 'Underscore di floating point: ';
+var_dump(1_234.567);
